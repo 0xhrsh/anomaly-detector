085c9e3c1453a1d3664f836653cb4df61cc02c94
harsh-not-haarsh <anand.2@iitj.ac.in>
2020-06-15 14:41:47 +0530

    checking before type asserting

--- hermes.go
+++ hermes.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
+	"errors"
 	"net/http"
 	"net/url"
 	"path"
@@ -37,20 +37,28 @@ func (svc hermes) CodeChanges(date time.Time) ([]CommitInfo, error) {
 	if err != nil {
 		return commits, nil
 	}
-	allCommits := res.(map[string]interface{})["values"].([]interface{})
+	if allCommits, ok := res.(map[string]interface{})["values"].([]interface{}); ok {
+		for i := range allCommits {
+			var temp CommitInfo
 
-	for i := range allCommits {
-		var temp CommitInfo
-		temp.Author = allCommits[i].(map[string]interface{})["author"].(map[string]interface{})["raw"].(string)
-		temp.Message = strings.Split(allCommits[i].(map[string]interface{})["message"].(string), "\n")[0]
-		temp.Date, _ = time.Parse(time.RFC3339, allCommits[i].(map[string]interface{})["date"].(string))
+			if tempAuthor, ok := allCommits[i].(map[string]interface{})["author"].(map[string]interface{})["raw"].(string); ok {
+				temp.Author = tempAuthor
+			}
+			if tempMessage, ok := allCommits[i].(map[string]interface{})["message"].(string); ok {
+				temp.Message = strings.Split(tempMessage, "\n")[0]
+			}
+			if tempDate, ok := allCommits[i].(map[string]interface{})["date"].(string); ok {
+				temp.Date, _ = time.Parse(time.RFC3339, tempDate)
+			}
 
-		if temp.Date.After(date.AddDate(0, 0, -2)) && temp.Date.Before(date.AddDate(0, 0, 1)) {
-			commits = append(commits, temp)
+			if temp.Date.After(date.AddDate(0, 0, -2)) && temp.Date.Before(date.AddDate(0, 0, 1)) {
+				commits = append(commits, temp)
+			}
 		}
+		return commits, nil
 	}
 
-	return commits, nil
+	return commits, errors.New("Error in unmarshalling bitbucket response")
 }
 
 func (svc hermes) SystemChanges(date time.Time) ([]Activity, error) {
@@ -81,7 +89,6 @@ func (svc hermes) SystemChanges(date time.Time) ([]Activity, error) {
 	req.Header.Set("Auth-Token", svc.config.AuthToken)
 	req.Header.Set("DNT", "1")
 
-	fmt.Println(req)
 	var client http.Client
 
 	res, err := client.Do(req)
